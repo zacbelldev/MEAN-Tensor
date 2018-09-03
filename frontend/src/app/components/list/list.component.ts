ab4026ea58b80e1c89bd3472cac9b1a48294657f
Zac Bell <zchrbell7@gmail.com>
2018-09-03 16:32:28 -0600

    this is awesome

--- frontend/src/app/components/list/list.component.ts
+++ frontend/src/app/components/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IssueService } from '../../issue.service';
+ 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private issueService: IssueService) { }
 
   ngOnInit() {
+    this.issueService.getIssues().subscribe((issues) => {
+      console.log(issues);
+    });
   }
 
 }
